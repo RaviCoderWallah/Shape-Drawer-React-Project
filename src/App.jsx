0aa4ae87c14aa386de5c5eac94929efc9ee2de7f
RaviCoderWallah <raviverma05042023@gmail.com>
2026-04-08 09:39:55 +0530

    feat: shape draw functionality and undo features completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
+import { useState } from 'react';
 import './App.css'
 
 function App() {
+
+  const [allCoordinates, setAllCoordinates] = useState([]);
+  const [showMessage, setShowMessage] = useState(true);
+  const [showDraw, setShowDraw] = useState(false);
+
+  const handleElementPosition = (event) => {
+
+    setShowMessage(false);
+    setShowDraw(true);
+
+    const container = event.currentTarget;
+    const rect = container.getBoundingClientRect();
+
+    const newTop = event.clientY - rect.y;
+    const newLeft = event.clientX - rect.x;
+
+    setAllCoordinates((prev) => [...prev, { top: newTop, left: newLeft, id: crypto.randomUUID() }]);
+  }
+
+  //For Clear Drawer 
+  const handleClearDraw = () => {
+    setAllCoordinates([]);
+    setShowMessage(true);
+  }
+
+  //For Undo Drawer 
+  const handleUndoDraw = () => {
+
+    setAllCoordinates((prev) => {
+      if (prev.length === 0) return prev;
+      return prev.slice(0, -1);
+    })
+  }
+
+  //For Redo Drawer
+  const handleRedoDraw = () => {
+
+  }
 
   return (
     <>
       <section className='max-w-4xl mx-auto py-8 flex flex-col items-center gap-8'>
         <h1 className='text-2xl text-center'>Shape Drawer</h1>
+
         <div className='flex items-center gap-4'>
-          <button className='p-1 px-4 bg-blue-500 text-white rounded-sm'>Clear</button>
-          <button className='p-1 px-4 bg-gray-300 text-white rounded-sm'>Undo</button>
-          <button className='p-1 px-4 bg-gray-300 text-white rounded-sm'>Redo</button>
+          <button className='p-1 px-4 bg-blue-500 text-white rounded-sm' onClick={handleClearDraw}>Clear</button>
+          <button className={`p-1 px-4 ${allCoordinates.length === 0 ? "bg-gray-300 cursor-no-drop" : "bg-blue-500 cursor-pointer"}  text-white rounded-sm`} onClick={handleUndoDraw}>Undo</button>
+          <button className='p-1 px-4 bg-gray-300 text-white rounded-sm' onClick={handleRedoDraw}>Redo</button>
           <select name="" id="" className='outline-1 p-1 px-4 rounded-sm'>
             <option value="circle">Circle</option>
             <option value="square">Square</option>
@@ -17,9 +57,24 @@ function App() {
           <input type="color" value="#0000FF" />
           <input type="range" />
         </div>
-        <div className='outline-dashed outline-2 outline-blue-500 w-full h-80 rounded-md flex items-center justify-center'>
-          <p className='text-xl text-center text-gray-600'>Click anywhere in this area to add a circle. <br/> Use Ctrl+Z to undo and Ctrl+Y to redo.</p>
+
+        <div
+          onClick={handleElementPosition}
+          className='relative outline-dashed outline-2 outline-blue-500 w-full h-80 rounded-md flex items-center justify-center overflow-hidden'
+        >
+          {
+            showMessage &&
+            <p className='text-xl text-center text-gray-600 pointer-events-none'>Click anywhere in this area to add a circle. <br /> Use Ctrl+Z to undo and Ctrl+Y to redo.</p>
+          }
+          {
+            (showDraw && allCoordinates.length !== 0) &&
+            allCoordinates.map((item) => {
+              return <div key={item.id} className={`w-8 h-8 bg-red-500 absolute`} style={{ top: item.top - 16, left: item.left - 16 }}></div>
+            })
+          }
+
         </div>
+
       </section>
     </>
   )
